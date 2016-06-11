ba21bb1cdb89ec8800552c6a69ccdb368ae08b0f
Jesse Stevenson <jessedusty@gmail.com>
2016-06-11 02:14:01 -0400

    got label template to work, added create label interface

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
 import logging
 import uuid
 
 from logging.handlers import RotatingFileHandler
+
+import model
 
 app = Flask(__name__)
 handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
@@ -21,11 +25,25 @@ app.secret_key = 'development key'
 HOST_URL = "http://192.168.99.100/"
 
 @app.route('/key/<identifer>')
-def show_user_profile(identifer):
+def show_object_for_key(identifer):
     # show the user profile for that user
-	return render_template("id.html", identifer=identifer)
+	if not model.itemExists(identifer):
+		return redirect(url_for("insert_item", identifer=identifer))
 
+	return render_template("id.html", info=model.infoForItem(identifer))
     #return 'User %s' % identifer
+
+@app.route("/add", methods=["GET", "POST"])
+def add_unknown_item():
+	if request.method == "POST":
+		return "inserted data into database, would you like to print label?"
+	else:
+		return "new label dialog"
+
+@app.route('/add/<identifer>')
+def insert_item(identifer):
+	#return "adding " + str(identifer)
+	return render_template("create.html", identifer=identifer)
 
 @app.route("/hello")
 def hello():
@@ -34,6 +52,10 @@ def hello():
 @app.route("/")
 def home():
 	return str("home")
+
+@app.route("/print/<identifer>")
+def generate_label(identifer):
+	return render_template("label.txt", info=model.infoForItem(identifer))
 
 if __name__ == "__main__":
     app.run()
--- model.py
+++ model.py
@@ -5,9 +5,15 @@ client = MongoClient("mongodb://db")
 db = client.stuff
 
 def infoForItem(identifer):
-    index =  db.items.find({"key":identifer})
+    index = db.items.find({"key":identifer})
+    if index.count() <= 0:
+        return None
     return index[0]
 
+def itemExists(identifer):
+    index = db.items.find({"key":identifer})
+    if index.count() > 0: return True
+    return False
 
 def insertItem(info):
     db.items.insert_one(info)
