692fc03ee180325e3bc5d741b2a7995cea0bc440
Jesse Stevenson <jessedusty@gmail.com>
2016-06-21 23:10:07 -0400

    autostart mongod container if not already running

--- runit.sh
+++ runit.sh
@@ -2,4 +2,5 @@
 rm foo.log* # clear logs
 docker rm -f mongo-flask # remove old container - if any
 # docker run -d --name=mongod mongo
+docker start mongod
 docker run -p 80:80 --rm=true --link mongod:db --name=mongo-flask -v `pwd`:/usr/src/app jessedusty/mongo-flask:latest # start new container
